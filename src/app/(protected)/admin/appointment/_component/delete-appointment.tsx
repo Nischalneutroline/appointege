f40071135323b4621554b63715652e49ef18c452
samratneutroline <samrat.neutroline@gmail.com>
2025-06-22 10:23:13 +0545

    Reverted The deleted files

--- src/app/(protected)/admin/appointment/_component/delete-appointment.tsx
+++ src/app/(protected)/admin/appointment/_component/delete-appointment.tsx
@@ -1,4 +1,4 @@
-import { Dialog, DialogContent } from '@/components/ui/dialog'
+import { Dialog, DialogContent, DialogTitle } from '@/components/ui/dialog'
 import { DialogDescription } from '@radix-ui/react-dialog'
 import Image from 'next/image'
 import React from 'react'
@@ -13,25 +13,27 @@ const DeleteAppointment = ({ open, onChange }: DeleteAppointmentProps) => {
   return (
     <Dialog onOpenChange={onChange} open={open}>
       <DialogContent>
-        <div className="flex justify-center">
-          <Image
-            src="/assets/Delete Empty state.svg"
-            alt="delete icon"
-            width={150}
-            height={150}
-          />
-        </div>
+        <DialogTitle>
+          <div className="flex justify-center">
+            <Image
+              src="/assets/Delete Empty state.svg"
+              alt="delete icon"
+              width={150}
+              height={150}
+            />
+          </div>
+        </DialogTitle>
         <DialogDescription className="text-center text-sm font-medium text-[#6B7280]">
           Are you sure you want to delete an appointment?
         </DialogDescription>
         <div className="flex w-full gap-4 justify-center">
           <button
             type="button"
-            className="flex justify-center items-center px-4 py-1.5 bg-[#EF4444] text-white rounded-md text-sm font-semibold"
+            className="flex justify-center items-center px-4 py-1.5 bg-[#EF4444] text-white rounded-md text-sm font-semibold cursor-pointer"
           >
             Delete
           </button>
-          <button className="flex justify-center items-center px-4 py-1.5 border border-[#E5E7EB] text-[#9CA3AF] rounded-md text-sm font-semibold">
+          <button className="flex justify-center items-center px-4 py-1.5 border border-[#E5E7EB] text-[#9CA3AF] rounded-md text-sm font-semibold cursor-pointer">
             Cancel
           </button>
         </div>
