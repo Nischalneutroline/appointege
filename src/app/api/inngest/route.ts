0c082dfabac374514f9bccfff55b23f5216a34c4
Nischal Puri <nischal1.neutroline@gmail.com>
2025-06-23 14:08:35 +0545

    deploy setup

--- src/app/api/inngest/route.ts
+++
@@ -1,9 +0,0 @@
-import { serve } from "inngest/next"
-import { appointmentReminder } from "@/tasks/inngest/function"
-import { inngestClient } from "@/tasks/inngest/client"
-import { sendAnnouncement } from "@/tasks/inngest/offerFunction"
-
-export const { GET, POST, PUT } = serve({
-  client: inngestClient,
-  functions: [appointmentReminder,/* sendAnnouncement */],
-})
