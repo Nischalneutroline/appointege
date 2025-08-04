744889096f84baac437137aa5b63e64bb2d5686e
Nischal Puri <nischal1.neutroline@gmail.com>
2025-08-04 18:22:21 +0545

    reminder form half completed

---
+++ src/app/(protected)/admin/reminders/layout.tsx
@@ -0,0 +1,31 @@
+'use client'
+import PageTabs from '@/components/admin/shared/page-tabs'
+import { ReminderTab, setActiveTab } from '@/store/slices/reminderSlice'
+import { RootState } from '@/store/store'
+import { useDispatch, useSelector } from 'react-redux'
+
+const Layout = ({ children }: { children: React.ReactNode }) => {
+  const { activeTab, activeTabs } = useSelector(
+    (state: RootState) => state.reminder,
+  )
+  const dispatch = useDispatch()
+  return (
+    <div className="flex gap-4 overflow-hidden flex-row bg-[#F8F9FA] h-full flex-1">
+      {' '}
+      {/* Use h-full and flex-1 */}
+      <div className="flex-1 w-full border rounded-md shadow h-full bg-white max-w-full flex flex-col overflow-y-auto">
+        {' '}
+        {/* Enable scrolling */}
+        <PageTabs<ReminderTab>
+          activeTab={activeTab}
+          onTabChange={(tab) => dispatch(setActiveTab(tab))}
+          customTabs={activeTabs}
+          className="p-6 py-3 border-b"
+        />
+        {children}
+      </div>
+    </div>
+  )
+}
+
+export default Layout
--- src/components/admin/shared/heading.tsx
+++ src/components/admin/shared/heading.tsx
@@ -1,15 +1,18 @@
+import { LucideIcon } from 'lucide-react'
 import React from 'react'
 
 interface HeadingProps {
   title: string
   description: string
+  icon?: LucideIcon
 }
 
-const Heading = ({ title, description }: HeadingProps) => {
+const Heading = ({ title, description, icon: Icon }: HeadingProps) => {
   return (
-    <div className="  text-[#2563EB]  ">
+    <div className=" text-[#2563EB]  ">
       <div className="flex items-center gap-2 ">
-        <span className=" text-2xl lg:text-3xl text-start font-semibold ">
+        <span className=" flex gap-2 items-center text-2xl lg:text-3xl text-start font-semibold ">
+          {Icon && <Icon className="size-6 text-[#6AA9FF]" />}
           {title}
         </span>
       </div>
